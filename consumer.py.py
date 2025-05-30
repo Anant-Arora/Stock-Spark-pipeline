f293fcd4e45d6c142c57698f4c436b44eda5da31
Anant Arora <anantarora2005@gmail.com>
2025-05-30 11:26:03 +0530

    Update consumer.py.py

--- consumer.py.py
+++ consumer.py.py
@@ -1,19 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[6]:
-
 
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType, StringType, FloatType
 
-# 1. Spark session
 spark = SparkSession.builder \
     .appName("KafkaStockConsumer") \
     .getOrCreate()
 
-# 2. Define schema
 schema = StructType() \
     .add("timestamp", StringType()) \
     .add("open", FloatType()) \
@@ -22,7 +15,6 @@ schema = StructType() \
     .add("close", FloatType()) \
     .add("volume", FloatType())
 
-# 3. Read from Kafka
 df = spark.readStream \
     .format("kafka") \
     .option("kafka.bootstrap.servers", "localhost:9092") \
@@ -30,25 +22,13 @@ df = spark.readStream \
     .option("startingOffsets", "latest") \
     .load()
 
-# 4. Convert value to string → JSON → extract fields
 df_parsed = df.selectExpr("CAST(value AS STRING)") \
     .select(from_json(col("value"), schema).alias("data")) \
     .select("data.*")
 
-# 5. Output to console
 query = df_parsed.writeStream \
     .outputMode("append") \
     .format("console") \
     .start()
 
 query.awaitTermination()
-
-
-
-
-
-# In[ ]:
-
-
-
-
